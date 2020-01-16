67a1d66c90086a2588c6a2c94419fa9a775f73ca
jacksongabrel88 <jacksongabriel88@gmail.com>
2020-01-16 10:56:18 -0300

    teste de uso da URL

--- PluriGo/src/app/now-playing.service.ts
+++ PluriGo/src/app/now-playing.service.ts
@@ -5,11 +5,15 @@ import { HttpClient } from  '@angular/common/http';
   providedIn: 'root'
 })
 export class NowPlayingService {
-  filmesURL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=37cd551d86fb77d5d2b7384b4cd9f0c3&language=en-US&page=1';
+  filmesURL:any;
  
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.filmesURL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=37cd551d86fb77d5d2b7384b4cd9f0c3&language=en-US&page=1';
+    console.log(this.filmesURL);
+    console.log(this.listar());
+   }
 
   listar(){
-    return this.http.get<any[]>('$this.filmesUrl')
+    return this.http.get<any[]>(this.filmesURL);
   };
 }
