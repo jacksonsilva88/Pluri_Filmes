c0a473d866f4a18849cbdd7d95140340c72ace03
jacksongabrel88 <jacksongabriel88@gmail.com>
2020-01-16 19:04:45 -0300

    inclusão do Cabeçalho como Layout

--- PluriGo/src/app/app.module.ts
+++ PluriGo/src/app/app.module.ts
@@ -2,19 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { routing } from './app.routing';
 
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { MoviesComponent } from './movies/movies.component';
-import { NowPlayingService }  from './now-playing.service';
-import { HttpClientModule } from '@angular/common/http';
-
-import {MatButtonModule} from '@angular/material/button';
-import {MatTooltipModule} from '@angular/material/tooltip';
 import { DetalhesComponent } from './movies/detalhes/detalhes.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NowPlayingService }  from './now-playing.service';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { FooterComponent } from './footer/footer.component';
     HttpClientModule,
     MatButtonModule,
     MatTooltipModule,
+    MatToolbarModule,
     routing,
   ],
   exports:[
